72f8cbbdfd8562cf1509a3e73ee501426b13e4fb
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-08 20:29:14 +0530

    Updates

---
+++ static/js/48.819aadd7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfoodorder=self.webpackChunkfoodorder||[]).push([[48],{48:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});var o=s(184);const d=()=>(0,o.jsx)("div",{className:"pt-44 bg-red-300  h-screen font-serif",children:(0,o.jsx)("h1",{children:"Our grocery online store....!!!"})})}}]);
+//# sourceMappingURL=48.819aadd7.chunk.js.map
